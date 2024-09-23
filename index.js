3464e3519199415abb21755c4a6d03833b8b5d5d
hriday <alauddinhriday@gmail.com>
2024-09-23 18:44:24 +0600

    Added some funtionalities

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const donationCardSection = document.getElementById('donation-card-section');
 
 // console.log(donationHistorySection, donationCardSection);
 
+// ....Click Handler for Donation and History Button.....
+
 donationBtn.addEventListener('click', function () {
     donationHistorySection.classList.add('hidden');
     donationHistorySection.classList.add('flex');
     donationCardSection.classList.remove('hidden');
+    donationCardSection.classList.add('flex');
 
     historyBtn.classList.remove('bg-green-500');
     donationBtn.classList.add('bg-green-500');
@@ -28,6 +31,76 @@ historyBtn.addEventListener('click', function () {
 
 });
 
+//  Multi Uses function for input by ID.....
 
+function inputValueById(id) {
+    const inputValue = document.getElementById(id).value;
+    const inputValueNum = parseFloat(inputValue);
+    return inputValueNum;
+};
+
+
+let donationAmount = 0;
+let donationAmount2 = 0;
+let donationAmount3 = 0;
+
+// ......Donation For Noakhali Part.........
+
+document.getElementById('donation-for-noakhali-btn').addEventListener('click', function () {
+    const donationValue = inputValueById('donation-for-noakhali');
+    
+    if (isNaN(donationValue) || typeof donationValue !== "number" || donationValue <= 0) {
+        return alert('invalid');
+    }
+
+    document.getElementById('noakhali-amount').innerText = donationAmount += donationValue;
+    const text = 'Taka is Donated for Flood at Noakhali, Bangladesh';
+    history(donationHistorySection, donationValue, text);
+    document.getElementById('donation-for-noakhali').value = '';
+});
+
+// ......Donation For Feni Part.........
+
+document.getElementById('donation-for-feni-btn').addEventListener('click', function () {
+    const donationValue = inputValueById('donation-for-feni');
+    
+    if (isNaN(donationValue) || typeof donationValue !== "number" || donationValue <= 0) {
+        return alert('invalid');
+    }
+
+    document.getElementById('feni-amount').innerText = donationAmount2 += donationValue;
+    const text = 'Taka is Donated for Flood at Feni, Bangladesh';
+    history(donationHistorySection, donationValue, text);
+    document.getElementById('donation-for-feni').value = '';
+});
+
+// ......Donation For Quota Movement Injured people Part.........
+
+document.getElementById('donation-for-injured-btn').addEventListener('click', function () {
+    const donationValue = inputValueById('donation-for-injured');
+    
+    if (isNaN(donationValue) || typeof donationValue !== "number" || donationValue <= 0) {
+        return alert('invalid');
+    }
+
+    document.getElementById('donation-for-injured-amount').innerText = donationAmount3 += donationValue;
+    const text = 'Taka is Donated for injured in the Quota Movement';
+    history(donationHistorySection, donationValue, text);
+    document.getElementById('donation-for-injured').value = '';
+    
+});
+
+// .......Reusable funtion for histoy........
+
+function history(section, donationMoney,text) {
+    const date = new Date();
+
+    section.innerHTML += `
+    <div class="p-4 border-2 rounded-xl">
+        <h1 class="text-xl font-bold">${donationMoney} ${text}</h1>
+        <p class="mt-3 p-2 bg-slate-200 rounded-md text-black">${date}</p>
+    </div>
+    `;
+}
 
 
